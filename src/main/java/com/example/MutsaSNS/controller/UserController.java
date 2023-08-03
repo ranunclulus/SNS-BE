87be911eb4bad7bad0cad8a1cd8958fd63009aab
ranunclulus <morion002@gmail.com>
2023-08-03 10:25:37 +0900

    feat: create userController and ResponseDto

---
+++ src/main/java/com/example/MutsaSNS/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.MutsaSNS.controller;
+
+import com.example.MutsaSNS.dtos.ResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/signup")
+    public ResponseDto signUp() {
+
+    }
+}
---
+++ src/main/java/com/example/MutsaSNS/dtos/ResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.MutsaSNS.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    private String message;
+}
