6be0ee7ff94638c9b473b0048025b941045b54d3
ranunclulus <morion002@gmail.com>
2023-08-03 11:30:38 +0900

    update: passwordEncoder Bean 추가:

--- src/main/java/com/example/MutsaSNS/config/WebSecurityConfig.java
+++ src/main/java/com/example/MutsaSNS/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
@@ -33,5 +34,10 @@ public class WebSecurityConfig {
                         AuthorizationFilter.class
                 );
         return httpSecurity.build();
+    }
+
+    @Bean
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
